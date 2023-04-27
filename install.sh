01ab992197a031a557cf566b62909278305fd983
aaronknudtson <aaron.knudtson@outlook.com>
2023-04-26 20:39:19 -0400

    fixed install script

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ -d ~/.config/nvim ]]; then
-    mv ~/.config/nvim ~/.config/nvim.backup
-    echo "moved existing nvim config to ~/.config/nvim.backup"
-fi
+backup () {
+    if [ -d $1 ] || [ -f $1 ]; then
+        mv $1 $1.backup;
+        echo "moved existing "$1" to "$1".backup";
+    fi
+    rm -rf $PATH;
+}
+
+# neovim
+backup ~/.config/nvim
+mkdir -p ~/.config
 ln -sf $(pwd)/init.lua ~/.config/nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-ln -sf .tmux.conf ~/.tmux.conf
+# tmux
+backup ~/.tmux.conf
+ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
+
