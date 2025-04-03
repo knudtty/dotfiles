e23b45e74cbe85be943a4ed6e708222607341f7b
Aaron Knudtson <aaron.knudtson@outlook.com>
2025-04-03 10:21:35 -0400

    dotfiles infinitely better. nvim updated to single file init.lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,537 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- OPTIONS
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+local options = {
+	backup = false, -- creates a backup file
+	cmdheight = 2, -- more space in the neovim command line for displaying messages
+	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+	conceallevel = 0, -- so that `` is visible in markdown files
+	fileencoding = "utf-8",
+	hlsearch = false, -- highlight all matches on previous search pattern
+	ignorecase = true, -- ignore case in search patterns
+	mouse = "", -- allow the mouse to be used in neovim
+	pumheight = 10, -- pop up menu height
+	showmode = false, -- we don't need to see things like -- INSERT -- anymore
+	showtabline = 2, -- always show tabs
+	smartcase = true, -- smart case
+	smartindent = false, -- make indenting smarter again
+	splitbelow = true, -- force all horizontal splits to go below current window
+	splitright = true, -- force all vertical splits to go to the right of current window
+	swapfile = false, -- creates a swapfile
+	termguicolors = true, -- set term gui colors (most terminals support this)
+	timeoutlen = 300, -- time to wait for a mapped sequence to complete (in milliseconds)
+	undofile = true, -- enable persistent undo
+	updatetime = 300, -- faster completion (4000ms default)
+	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	expandtab = true, -- convert tabs to spaces
+	shiftwidth = 4, -- the number of spaces inserted for each indentation
+	tabstop = 4, -- insert 4 spaces for a tab
+	softtabstop = 4,
+	cursorline = true, -- highlight the current line
+	relativenumber = true, -- set relative numbered lines
+	number = true,
+	numberwidth = 3, -- set number column width to 2 {default 4}
+	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+	wrap = false, -- display lines as one long line
+	scrolloff = 8, -- begin to scroll when cursor gets n lines from top of bottom
+	sidescrolloff = 8,
+	guicursor = "",
+	incsearch = true,
+	colorcolumn = "80",
+}
+vim.opt.shortmess:append("c")
+-- Apply Options
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+-- PLUGINS
+local plugins = {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		lazy = false,
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+				end,
+			},
+			{
+				"<leader>a",
+				function()
+					require("harpoon"):list():add()
+				end,
+			},
+			{
+				"<C-j>",
+				function()
+					require("harpoon"):list():select(1)
+				end,
+			},
+			{
+				"<C-k>",
+				function()
+					require("harpoon"):list():select(2)
+				end,
+			},
+			{
+				"<C-l>",
+				function()
+					require("harpoon"):list():select(3)
+				end,
+			},
+			{
+				"<C-;>",
+				function()
+					require("harpoon"):list():select(4)
+				end,
+			},
+		},
+		config = function()
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
+			vim.keymap.set("n", "<C-e>", function()
+				toggle_telescope(require("harpoon"):list())
+			end, { desc = "Open harpoon window" })
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		cmd = { "ConformInfo" },
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescript = { "prettierd" },
+				typescriptreact = { "prettierd" },
+			},
+		},
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true })
+				end,
+				desc = "Format file",
+			},
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+	},
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("lint").linters_by_ft = {
+				javascript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescript = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+			}
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		opts = {},
+	},
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lua",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+					{ name = "buffer" },
+					{ name = "nvim_lua" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+				}),
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
+					end,
+				},
+			})
+		end,
+	},
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		init = function()
+			-- Reserve a space in the gutter
+			-- This will avoid an annoying layout shift in the screen
+			vim.opt.signcolumn = "yes"
+		end,
+		config = function()
+			-- LspAttach is where you enable features that only work
+			-- if there is a language server active in the file
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local opts = { buffer = event.buf }
+
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+					vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+					vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+					vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+					--vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+					vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+				end,
+			})
+
+			local lsp_defaults = require("lspconfig").util.default_config
+			lsp_defaults.capabilities =
+				vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					-- this first function is the "default handler"
+					-- it applies to every language server without a "custom handler"
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })()
+		end,
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				-- A list of parser names, or "all"
+				ensure_installed = { "templ", "vimdoc", "javascript", "typescript", "c", "lua", "rust", "go", "bash" },
+
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
+
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = true,
+
+				highlight = {
+					-- `false` will disable the whole extension
+					enable = true,
+
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = false,
+				},
+			})
+
+			-- templ fixes
+			vim.filetype.add({ extension = { templ = "templ" } })
+			vim.api.nvim_create_autocmd("BufEnter", {
+				pattern = "*.templ",
+				callback = function()
+					vim.cmd("TSBufEnable highlight")
+				end,
+			})
+		end,
+	},
+	--{
+	--    "nvim-treesitter/nvim-treesitter-context"
+	--}
+	-- Colorschemes
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
+	{
+		"folke/tokyonight.nvim",
+	},
+	{
+		"catppuccin/nvim",
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			defaults = {
+				preview = {
+					mime_hook = function(filepath, bufnr, opts)
+						local is_image = function(filepath)
+							local image_extensions = { "png", "jpg" } -- Supported image formats
+							local split_path = vim.split(filepath:lower(), ".", { plain = true })
+							local extension = split_path[#split_path]
+							return vim.tbl_contains(image_extensions, extension)
+						end
+						if is_image(filepath) then
+							local term = vim.api.nvim_open_term(bufnr, {})
+							local function send_output(_, data, _)
+								for _, d in ipairs(data) do
+									vim.api.nvim_chan_send(term, d .. "\r\n")
+								end
+							end
+							vim.fn.jobstart({
+								"catimg",
+								filepath, -- Terminal image viewer command
+							}, { on_stdout = send_output, stdout_buffered = true, pty = true })
+						else
+							require("telescope.previewers.utils").set_preview_message(
+								bufnr,
+								opts.winid,
+								"Binary cannot be previewed"
+							)
+						end
+					end,
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>ps",
+				function()
+					require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+				end,
+			},
+			{
+				"<leader>pf",
+				function()
+					require("telescope.builtin").find_files()
+				end,
+			},
+			{
+				"<leader>pg",
+				function()
+					require("telescope.builtin").live_grep()
+				end,
+			},
+			{
+				"<leader>pb",
+				function()
+					require("telescope.builtin").buffers()
+				end,
+			},
+			{
+				"<leader>ph",
+				function()
+					require("telescope.builtin").help_tags()
+				end,
+			},
+			{
+				"<leader>pw",
+				function()
+					require("telescope.builtin").grep_string()
+				end,
+			},
+		},
+	},
+	{
+		"tpope/vim-fugitive",
+		cmd = "Git",
+	},
+	--{
+	--    "mbbill/undotree", -- investigate
+	--  keys = {
+	--  	{ "<leader>u", vim.cmd.UndotreeToggle },
+	--  },
+	--},
+	{
+		"folke/zen-mode.nvim", -- investigate
+		keys = {
+			{
+				"<leader>zz",
+				function()
+					require("zen-mode").setup({
+						window = {
+							width = 90,
+							options = {},
+						},
+					})
+					require("zen-mode").toggle()
+					vim.wo.wrap = false
+					vim.wo.number = true
+					vim.wo.rnu = true
+				end,
+			},
+			{
+				"<leader>zZ",
+				function()
+					require("zen-mode").setup({
+						window = {
+							width = 80,
+							options = {},
+						},
+					})
+					require("zen-mode").toggle()
+					vim.wo.wrap = false
+					vim.wo.number = false
+					vim.wo.rnu = false
+					vim.opt.colorcolumn = "0"
+				end,
+			},
+		},
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+}
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = plugins,
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+-- GENERAL KEYMAPS
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y') -- yank to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>Y", '"+Y') -- yank to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p') -- paste to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>P", '"+P') -- paste to system clipboard
+-- Navigate buffers
+--vim.keymap.set("n", "<S-l>", ":bnext<CR>")
+--vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
+-- close buffer
+vim.keymap.set("n", "<leader>c", "<cmd>bd!<CR>")
+-- oops
+vim.keymap.set("i", "<C-c>", "<Esc>")
+-- search and replace current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Open vim-fugitive
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- Color
+vim.cmd("colorscheme rose-pine")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- Autocmds
+local reducedTabFileTypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" }
+for _, ft in ipairs(reducedTabFileTypes) do
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = ft,
+		command = "setlocal shiftwidth=2 tabstop=2",
+	})
+end
